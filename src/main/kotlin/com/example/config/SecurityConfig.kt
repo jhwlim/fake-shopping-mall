45ed4660bb750f1d7e16121b31f0454ddca1e9d5
jhwlim <jhwlim25@gmail.com>
2022-08-18 23:21:20 +0900

    chore : 시큐리티 REST API 기본 설정

---
+++ src/main/kotlin/com/example/config/SecurityConfig.kt
@@ -0,0 +1,32 @@
+package com.example.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf().disable()
+
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+            .and()
+            .formLogin().disable()
+            .logout().disable()
+
+            .authorizeRequests()
+            .antMatchers(
+            )
+            .permitAll()
+            .anyRequest()
+            .authenticated()
+        return http.build()
+    }
+
+}
