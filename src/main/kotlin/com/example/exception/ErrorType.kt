68bf5bb63d69711dbecd0934e403a718b027d303
jhwlim <jhwlim25@gmail.com>
2022-08-20 18:59:42 +0900

    chore : validation 예외처리 추가

--- src/main/kotlin/com/example/exception/ErrorType.kt
+++ src/main/kotlin/com/example/exception/ErrorType.kt
@@ -12,6 +12,7 @@ enum class ErrorType(
     USER_NOT_FOUND(1002, "사용자 정보를 찾을 수 없습니다.", HttpStatus.UNAUTHORIZED),
     PASSWORD_NOT_MATCHES(1003, "패스워드 정보가 일치하지 않습니다.", HttpStatus.UNAUTHORIZED),
 
+    INVALID_REQUEST(9001, "유효하지 않은 요청입니다."),
     UNKNOWN(9999, "알 수 없는 오류가 발생했습니다.", HttpStatus.INTERNAL_SERVER_ERROR)
 
 }
