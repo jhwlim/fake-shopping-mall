d01eb804be1b6811edbf87cb54bab3e5c1d498e8
jhwlim <jhwlim25@gmail.com>
2022-08-20 13:05:33 +0900

    feat : UserRepository 이름으로 조회하기 추가

--- src/main/kotlin/com/example/domain/user/UserRepository.kt
+++ src/main/kotlin/com/example/domain/user/UserRepository.kt
@@ -2,4 +2,8 @@ package com.example.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface UserRepository : JpaRepository<User, Long>
+interface UserRepository : JpaRepository<User, Long> {
+
+    fun findByName(name: String): User?
+
+}
---
+++ src/test/kotlin/com/example/domain/user/UserRepositoryTest.kt
@@ -0,0 +1,50 @@
+package com.example.domain.user
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.shouldNotBe
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import java.time.LocalDateTime
+
+@DataJpaTest
+internal class UserRepositoryTest @Autowired constructor(
+    val userRepository: UserRepository,
+) : BehaviorSpec({
+
+    Given("findByName - 일치하는 이름의 사용자가 DB에 존재하는 경우") {
+        val name = "test"
+
+        When("사용자 정보를 조회하면") {
+            val actual = userRepository.findByName(name)
+
+            Then("해당 사용자 정보를 반환해야 한다.") {
+                actual shouldNotBe null
+                actual?.let {
+                    actual.id shouldBe 1L
+                    actual.name shouldBe "test"
+                    actual.password shouldBe "1234"
+                    actual.role shouldBe UserRole.USER
+                    actual.createdDateTime shouldBe LocalDateTime.of(2022, 8, 1, 0, 0, 0)
+                    actual.updatedDateTime shouldBe LocalDateTime.of(2022, 8, 1, 0, 0, 0)
+                }
+            }
+
+        }
+
+    }
+
+    Given("findByName - 일치하는 이름의 사용자가 DB에 존재하지 않는 경우") {
+        val name = "name"
+
+        When("사용자 정보를 조회하면") {
+            val actual = userRepository.findByName(name)
+
+            Then("null을 반환해야 한다.") {
+                actual shouldBe null
+            }
+
+        }
+    }
+
+})
